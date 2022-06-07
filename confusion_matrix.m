0813450b4a0a6cb5ca9a501e4c795d65a705b935
Gal1eo <roylandstinger@gmail.com>
2022-06-07 13:19:17 +0200

    confusion matrix

--- confusion_matrix.m
+++ confusion_matrix.m
@@ -1,9 +1,31 @@
-mat=load("/Volumes/zijianf/mat.txt");
-cor=load("/Volumes/zijianf/cor.txt");
-d_matrix=mat+diag(cor);
 phones=importdata("/Volumes/zijianf/phones.txt");
-sTable = array2table(d_matrix,'RowNames',phones,...
-    'VariableNames',phones);
+mat=load("/Volumes/zijianf/mat_noise.txt");
+cor=load("/Volumes/zijianf/cor_noise.txt");
+ins=load("/Volumes/zijianf/ins_noise.txt");
+del=load("/Volumes/zijianf/del_noise.txt");
+d_matrix=mat+diag(cor)+ins+del;
+%norm_mtr=d_matrix./total;
+%sTable = array2table(d_matrix,'RowNames',phones,...
+%    'VariableNames',phones);
 
-norm_mtr=d_matrix./sum(d_matrix,2);
-heatmap(phones,phones,norm_mtr);
+figure(1)
+norm_mtr_1=d_matrix./sum(d_matrix,2);
+heatmap(phones,phones,norm_mtr_1);
+
+
+mat=load("/Volumes/zijianf/mat_nor.txt");
+cor=load("/Volumes/zijianf/cor_nor.txt");
+ins=load("/Volumes/zijianf/ins_nor.txt");
+del=load("/Volumes/zijianf/del_nor.txt");
+d_matrix=mat+diag(cor)+ins+del;
+%norm_mtr=d_matrix./total;
+%sTable = array2table(d_matrix,'RowNames',phones,...
+%    'VariableNames',phones);
+
+figure(2)
+norm_mtr_2=d_matrix./sum(d_matrix,2);
+heatmap(phones,phones,norm_mtr_2);
+
+diff=norm_mtr_2-norm_mtr_1;
+figure(3)
+heatmap(phones,phones,log(abs(diff)+1e-8));
